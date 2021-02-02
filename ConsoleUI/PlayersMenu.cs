14300ae2911dee28a2074aacc00660d5522f8904
Hadeed <hadeedji@gmail.com>
2021-02-03 03:34:17 +0500

    Add a working menu

---
+++ ConsoleUI/PlayersMenu.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using ArtificialIntelligenceEngine;
+using TicTacToeEngine;
+
+namespace ConsoleUI {
+public class PlayersMenu {
+    private int selectedIndex { get; set; }
+    private Player[] players { get; set; }
+    private string[] playerNames { get; set; }
+    private Random randomNumberGenerator { get; set; }
+
+
+    public PlayersMenu() {
+        selectedIndex = 0;
+        players = new Player[2];
+        playerNames = new[] {"", ""};
+        randomNumberGenerator = new Random();
+    }
+
+    private void DisplayMenu() {
+        Console.Clear();
+        Console.CursorVisible = false;
+        Console.WriteLine("Welcome to TicTacToe!!\n");
+        for (var i = 0; i < playerNames.Length; i++) {
+            string currentlySelectedPlayer = playerNames[i];
+
+            if (i == selectedIndex) {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Black;
+            } else {
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine($"Player {i + 1}: {currentlySelectedPlayer}");
+        }
+
+        Console.ResetColor();
+    }
+
+    public Player[] GetPlayers() {
+        DisplayMenu();
+
+        var conditional = true;
+        while (conditional) {
+            var keyPressed = Console.ReadKey(true).Key;
+            switch (keyPressed) {
+                case ConsoleKey.DownArrow:
+                    if (selectedIndex < playerNames.Length - 1) {
+                        selectedIndex++;
+                        DisplayMenu();
+                    }
+
+                    break;
+                case ConsoleKey.UpArrow:
+                    if (selectedIndex > 0) {
+                        selectedIndex--;
+                        DisplayMenu();
+                    }
+
+                    break;
+                case ConsoleKey.H:
+                    players[selectedIndex] = new HumanPlayer();
+                    playerNames[selectedIndex] = "Human";
+                    DisplayMenu();
+                    break;
+                case ConsoleKey.A:
+                    players[selectedIndex] = new RandomPlayer(randomNumberGenerator);
+                    playerNames[selectedIndex] = "Artificial Intelligence";
+                    DisplayMenu();
+                    break;
+                case ConsoleKey.Enter:
+                    if (players[0] != null && players[1] != null) {
+                        conditional = false;
+                    }
+
+                    break;
+            }
+        }
+
+
+        return players;
+    }
+}
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,52 +1,12 @@
 ﻿using System;
-using TicTacToeEngine;
 
 namespace ConsoleUI {
 class Program {
     static void Main() {
-        Player player1 = new HumanPlayer(PromptAndInput("Enter player 1 name: "));
-        Player player2 = new HumanPlayer(PromptAndInput("Enter player 2 name: "));
-        var gameController = new GameController(player1, player2);
-
-        var result = gameController.StartGame();
-        string message = result switch {
-            Result.Draw => "The game was draw.",
-            Result.PlayerOneWon => $"{player1.name} won!!!",
-            Result.PlayerTwoWon => $"{player2.name} won!!!",
-            _ => throw new ArgumentOutOfRangeException()
-        };
-        Console.WriteLine(message);
-
-        Console.WriteLine($"{player1.name} wins: {gameController.player1Score}");
-        Console.WriteLine($"{player2.name} wins: {gameController.player2Score}");
-        Console.WriteLine($"Draws: {gameController.draws}");
-    }
-
-    static void Drawboard(Board board) {
-        Console.WriteLine("\n");
-        PrintRow(board.Row(0));
-        PrintRow(board.Row(1));
-        PrintRow(board.Row(2));
-    }
-
-    static void PrintRow(Cell[] row) {
-        Console.WriteLine($"[{CharacterFromCellState(row[0])} " +
-                          $"{CharacterFromCellState(row[1])} " +
-                          $"{CharacterFromCellState(row[2])}]");
-    }
-
-    static char CharacterFromCellState(Cell state) {
-        return state switch {
-            Cell.E => '_',
-            Cell.X => 'X',
-            Cell.O => 'O',
-            _ => throw new ArgumentOutOfRangeException(nameof(state), state, null)
-        };
-    }
-
-    static string PromptAndInput(string prompt) {
-        Console.Write(prompt);
-        return Console.ReadLine();
+        PlayersMenu menu = new PlayersMenu();
+        var players = menu.GetPlayers();
+        Console.WriteLine("Press enter to continue.");
+        Console.ReadKey();
     }
 }
 }
