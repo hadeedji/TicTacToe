577f9c96984fe0fb2ce4d6c89e49305f61bc88d6
Hadeed <hadeedji@gmail.com>
2021-02-03 21:20:06 +0500

    Add ConsoleInputController.cs

---
+++ ConsoleUI/ConsoleInputController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleUI {
+public class ConsoleInputController {
+    public class Keybind {
+        public ConsoleKey key { get; }
+        public Action action { get; }
+
+        public Keybind(ConsoleKey key, Action action) {
+            this.key = key;
+            this.action = action;
+        }
+    }
+
+    private List<Keybind> keybinds { get; }
+
+    public ConsoleInputController() {
+        keybinds = new List<Keybind>();
+    }
+    
+    public void AddKeybind(ConsoleKey key, Action action) {
+        keybinds.Add(new Keybind(key, action));
+    }
+
+    //TODO: Inefficient function. Fix when you learn better LINQ
+    public void Run() {
+        ConsoleKey key;
+        do {
+            key = Console.ReadKey(true).Key;
+        } while (keybinds.Select(keybind => keybind.key).Contains(key));
+
+        foreach (Keybind keybind in keybinds.Where(keybind => keybind.key == key)) {
+            keybind.action.Invoke();
+        }
+    }
+}
+}
